68e79281d3edd2729b46ae48e9d4b1e3d05595e5
Vesela Apostolova <100559743+vap0204@users.noreply.github.com>
2022-12-10 22:33:24 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,31 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  // ======================================================= //
+
+  const request = new Request("https://pokeapi.co/api/v2/pokemon?limit=10");
+  fetch(request)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("Something went wrong on API server!");
+      }
+    })
+    .then((data) => {
+      const pokemon = data.results.map((p) => {
+        // return `<li>${p.name}</li>`;
+        return p.name;
+      });
+
+      pokemon.forEach((name) => {
+        const li = document.createElement("li");
+        li.innerText = name;
+        ul.appendChild(li);
+        console.log(name);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
